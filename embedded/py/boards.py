8a27754353b406247fc4df5f3994e5ad172b2939
Vaughn Kottler <vkottler@wisc.edu>
2018-02-24 18:45:34 -0600

    working on the board discovery command

--- embedded/py/boards.py
+++ embedded/py/boards.py
@@ -3,12 +3,26 @@
 Badgerloop - Board Discovery
 """
 
+# built-in
+import subprocess
+import json
+
 # parse result of `lsblk -fJ`
 
 def run(args):
     """ """
 
-    print "boards"
+    board_count = 0
+    lsblk_raw = subprocess.check_output(["lsblk", "-fJ"])
+    lsblk_data = json.loads(lsblk_raw)
+
+    for device in lsblk_data["blockdevices"]:
+        if device["label"] and "NODE_" in device["label"]:
+            print device["label"]
+            board_count += 1
+
+    print "Boards found: %d" % board_count
+
     return 0
 
 def init_args(parser):
