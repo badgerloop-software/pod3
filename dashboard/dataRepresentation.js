298bc71ff35705de7a4de4b58a2b9888fdcf36f4
Ezra Boley <eboley@wisc.edu>
2018-03-24 12:42:12 -0500

    Started making a graph using graphjs

---
+++ dashboard/dataRepresentation.js
@@ -0,0 +1,67 @@
+const canvas = document.getElementById("pressure-graph").getContext('2d');
+const charts = require("chart.js");
+
+// Update X as the t value
+// Update Y as value at that t value
+let dt = 0;
+let newChart = new charts.Chart(canvas, {
+    type: 'line',
+    data: {
+        labels: ["Pressure"],
+        datasets: [{
+            label: 'Pressure',
+            data:[{
+                t: dt++,
+                y: 10,
+            }],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+            ],
+            borderWidth: 1,
+            fill: false,
+        }]
+    },
+    options: {
+        scales: {
+            xAxes: [{
+                type: 'time',
+                time:{
+                   unit:"millisecond"
+                },
+                distribution: 'series',
+
+                ticks: {
+                    beginAtZero: true,
+                    stepSize: .5
+                },
+            }],
+            yAxes: [{
+                type: 'linear',
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
+/**@name addData
+ * @purpose Takes in a label for the new data and some new information to
+ * add to the graph and updates the graph with that info
+ *
+ * @param label the tag for the new piece of data (might go away)
+ * @param data the actual data point, should be in JSON form e.g. { x:number, y:number }
+ *
+ **/
+function addData(chart, label, data) {
+    //newChart.data.labels.push(label);
+    newChart.data.datasets.forEach((dataset) => {
+        dataset.data.push(data);
+    })
+    chart.update();
+}
+
+module.exports.addData = addData;
