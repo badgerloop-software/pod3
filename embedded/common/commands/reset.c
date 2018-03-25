9899a58315168b5f22d560f0b6817e27e6605021
Vaughn Kottler <vkottler@wisc.edu>
2018-03-25 14:43:42 -0500

    implement reset command

--- embedded/common/commands/reset.c
+++ embedded/common/commands/reset.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 #include "commands.h"
 
+#include "stm32l432xx.h"
+#include "core_cm4.h"
+
+#define UNUSED(x) (void)(x)
+
 command_status do_reset(int argc, char *argv[]) {
-	printf("%s: TODO (%d args given)\r\n", argv[0], argc);
+	UNUSED(argc); UNUSED(argv);
+	NVIC_SystemReset();
 	return SUCCESS;
 }
 
-COMMAND_ENTRY(
-	"reset",
-	"reset",
-	"Perform a software reset.",
-	do_reset
-)
+COMMAND_ENTRY("reset", "reset", "Perform a software reset.", do_reset)
