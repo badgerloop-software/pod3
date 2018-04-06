a97cbcaaf0f8867777751628989d529a47bc553c
Ethan Link <ethanlink17@live.com>
2018-04-05 22:04:29 -0500

    Set up some functon headers for I2C

--- embedded/drivers/i2c.c
+++ embedded/drivers/i2c.c
@@ -52,22 +52,22 @@ bool I2C_Transmit(uint32_t i2c_base,
 
 
 /*
- *I2C_transmit_receive_config
+ *I2C_Receive
  *
- *	Should call I2C_Master_Transmit_Byte for each byte of data (nBytes times).
+ *	Should call I2C_Master_Receive_Byte for each byte of data (nBytes times).
  *	Each byte of data is stored as an entry in the data_buffer.
  *
 */
-bool I2C_Transmit(uint32_t i2c_base, 
+bool I2C_Receive(uint32_t i2c_base, 
 		uint32_t nBytes, uint8_t* data_buffer){
 
 }	
 
 
 /*
- *I2C_Master_Receive_Byte
+ *I2C_clockConfig
  *	
- *	Reads one byte from the slave.
+ *	Initializes the clock
  *
 */
 bool I2C_clockConfig(uint32_t i2c_base, bool state ){
@@ -86,7 +86,28 @@ bool I2C_enable(bool enable, uint32_t i2c_base){
 }
 
 
+
+
+/*
+ *I2C_disable_NVIC
+ *	
+ *	Disable interrupts for Atomic operation.
+ *
+*/
 bool I2C_disable_NVIC(uint32_t i2c_base){
 
 }
+
+
+/*
+ *I2C_enable_NVIC
+ *	
+ *	Enable interrupts to end Atomic operation.
+ *
+*/
+bool I2C_enable_NVIC(uint32_t i2c_base){
+
+}
+
+
 
