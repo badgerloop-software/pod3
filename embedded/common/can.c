c72a54df41844526bff340d961db99aea90a2d68
Ryan Castle <rcastle@wisc.edu>
2018-06-09 19:43:44 +0000

    OBD2 BMS format

--- embedded/common/can.c
+++ embedded/common/can.c
@@ -35,7 +35,7 @@ void can_send(uint16_t can_id, size_t length, uint8_t *TxData, CAN_HandleTypeDef
 	TxHeader.RTR = 0;
 	TxHeader.DLC = length;
 	if(HAL_CAN_GetTxMailboxesFreeLevel(hcan)){
-		printf("SENDING MESSAGE");
+		printf("SENDING MESSAGE\r\n");
 	 	/*
 		TxData[0] = (uint8_t) 0x55;
 		TxData[1] = (uint8_t) 0x55;
@@ -49,13 +49,13 @@ void can_send(uint16_t can_id, size_t length, uint8_t *TxData, CAN_HandleTypeDef
 		uint32_t TxMailbox = 0;
 		
 		if(HAL_CAN_AddTxMessage(hcan, &TxHeader, TxData, &TxMailbox)!= HAL_OK){
-			printf("ADD TX MESSAGE ERROR");
+			printf("ADD TX MESSAGE ERROR\r\n");
 
 		} else {
-			printf("SEND OK");
+			printf("SEND OK\r\n");
 		}
 
-		printf("TxMailbox after sending %lu",TxMailbox);
+		printf("TxMailbox after sending %lu\r\n",TxMailbox);
 	}
 	/* LOOPBACK MODE STUFF
 	for(int i = 0; i < 1000; i++){
@@ -76,3 +76,93 @@ void can_send(uint16_t can_id, size_t length, uint8_t *TxData, CAN_HandleTypeDef
 	}
 	*/
 }
+
+
+
+uint8_t * can_send_obd2(uint16_t can_id, size_t message_length, uint8_t mode, uint16_t pid, CAN_HandleTypeDef *hcan){
+	
+	TxHeader.StdId = can_id;
+	TxHeader.IDE = 0;
+	TxHeader.RTR = 0;
+	TxHeader.DLC = (uint8_t) 8;
+	// Required message format:
+	// Number of additional bytes:
+	// mode:
+	// pid code:
+	
+	uint8_t TxData[8];
+	TxData[0] = message_length;
+	TxData[1] = mode;
+	TxData[2] = pid;
+	TxData[3] = 0x55;
+	TxData[4] = 0x55;
+	TxData[5] = 0x55;
+	TxData[6] = 0x55;
+	TxData[7] = 0x55;
+	
+	if(HAL_CAN_GetTxMailboxesFreeLevel(hcan)){
+		printf("SENDING MESSAGE\r\n");
+	 	/*
+		TxData[0] = (uint8_t) 0x55;
+		TxData[1] = (uint8_t) 0x55;
+		TxData[2] = (uint8_t) 0x55;
+		TxData[3] = (uint8_t) 0x55;
+		TxData[4] = (uint8_t) 0x55;
+		TxData[5] = (uint8_t) 0x55;
+		TxData[6] = (uint8_t) 0x55;
+		TxData[7] = (uint8_t) 0x55;
+		*/
+		uint32_t TxMailbox = 0;
+		
+		if(HAL_CAN_AddTxMessage(hcan, &TxHeader, TxData, &TxMailbox)!= HAL_OK){
+			printf("ADD TX MESSAGE ERROR\r\n");
+
+		} else {
+			printf("SEND OK\r\n");
+		}
+
+		printf("TxMailbox after sending %lu\r\n",TxMailbox);
+	}
+
+	
+	if(HAL_CAN_GetRxFifoFillLevel(hcan, CAN_RX_FIFO0)){
+		printf("CAN MESSAGE IN FIFO\r\n");
+		HAL_CAN_GetRxMessage(hcan, CAN_RX_FIFO0, &RxHeader, RxData);
+		
+		for(int i = 0; i < 8; i++){
+			if( RxData[i] != 0){
+				printf("Here is our message %x\r\n", RxData[i]);
+			}
+		}
+	}
+
+
+
+
+
+
+	return RxData;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
