c7bb995ba8e8fdfa6ba9196dd55d338992db3339
Finn Kuusisto <finn.kuusisto@gmail.com>
2018-07-07 13:20:04 -0500

    First pass at receiving sensor messages over UART.

--- server/pod_server/sensorsenderd.py
+++ server/pod_server/sensorsenderd.py
@@ -13,6 +13,11 @@ import json
 SENSOR_NAME = 'sensor_name'
 SENSOR_VALUE = 'value'
 TIMESTAMP = 'timestamp'
+UART_PORT = '/dev/ttyS0'
+UART_BAUD = 115200
+UART_CMD_START = '$|'
+
+SERIAL_PORT = serial.Serial(UART_PORT, baudrate=UART_BAUD)
 
 def build_arg_parser():
     parser = argparse.ArgumentParser(description='Read sensors on the pod and send them to the external server to store in the database.')
@@ -26,24 +31,58 @@ def build_udp_endpoint(send_ip, send_port, recv_ip, recv_port):
     ep = bloop_comms.udp_endpoint(send_ip, send_port, recv_ip, recv_port)
     return ep
 
-def read_sensor_data():
-    # TODO collect sensor data and convert to a json string
-    # TODO should all be coming as one big blob over SPI or I2C
+def read_until_newline():
+    mesg = ''
+    done = False
+    while not done:
+        c = SERIAL_PORT.read()
+        mesg += c
+        done = c == '\n'
+    return mesg
+
+def pull_command_from_message(mesg):
+    # we assume this is a \n terminated string
+    mesg = mesg.replace('\n', '')
+    # then look for rightmost the start of a command
+    cmd_start_ind = mesg.rfind(UART_CMD_START)
+    if cmd_start_ind < 0:
+        # no command start marker found
+        return None
+    # found a command so give it back
+    return mesg[cmd_start_ind:]
+
+def convert_command_to_json_payload(cmnd):
+    # first remove the command indicator
+    cmnd = cmnd.replace(UART_CMD_START, '')
+    # it's just a comma separated pair of name and value
+    parts = cmnd.split(',')
+    sensor_name = parts[0]
+    sensor_value = parts[1]
     # get the current seconds since epoch (use datetime if string preferred)
     curr_time = int(time.time())
-    # put together a dummy for testing
-    ret = dict()
-    ret[TIMESTAMP] = curr_time
-    ret[SENSOR_NAME] = random.choice(['sensor_1', 'sensor_2', 'sensor_3'])
-    ret[SENSOR_VALUE] = random.randint(0, 250)
-    time.sleep(1.0)
-    return json.dumps(ret)
+    # and put those into a dict for json translation
+    data = dict()
+    data[TIMESTAMP] = curr_time
+    data[SENSOR_NAME] = sensor_name
+    data[SENSOR_VALUE] = sensor_value
+    return json.dumps(data)
+
+def read_sensor_data():
+    # grab whatever we can off serial
+    mesg = read_until_newline()
+    # pull out the command data
+    cmnd = pull_command_from_message(mesg)
+    if cmnd is None:
+        return None
+    # and turn the command into a Mongo server message
+    return convert_command_to_json_payload(cmnd)
 
 def send_loop(udp_comm):
     # read and send forever
     while True:
         to_send = read_sensor_data()
-        udp_comm.send(to_send)
+        if to_send not None:
+            udp_comm.send(to_send)
 
 def main():
     # setup comms from the args
