1a2e8f2743059d94862840c37b53f8e98cb5bf7b
Ezra Boley <eboley@wisc.edu>
2018-07-07 23:45:39 +0000

    Working on formatting the outgoing messages from the nucleo, C strings are not very friendly. Currently prints (null)

--- embedded/include/uart.h
+++ embedded/include/uart.h
@@ -5,24 +5,7 @@
 #include <stm32l4xx_hal.h>
 #include <time.h>
 
-#define FRESH 		0
-#define NOT_FRESH 	1
-
 command_status uart_send();
 command_status uart_receive();
-
-typedef struct {
-	char *name;
-	uint8_t data;
-	time_t timestamp;
-	uint8_t freshness;
-} Sensor_Data;
-
-
-typedef struct {
-	Sensor_Data pressure;
-	Sensor_Data temperature;	
-} Pod_Data_Handle;
-
 
 #endif
