e45cdebfbd0896809b8869fa380f8c1680b302bb
Ezra Boley <eboley@wisc.edu>
2018-07-07 20:39:40 +0000

    Adding the framework for how we are going to process incoming sensor data and pass it off

--- embedded/include/uart.h
+++ embedded/include/uart.h
@@ -3,8 +3,26 @@
 #include "commands.h"
 #include "system.h"
 #include <stm32l4xx_hal.h>
+#include <time.h>
+
+#define FRESH 		0
+#define NOT_FRESH 	1
+
 command_status uart_send();
 command_status uart_receive();
+
+typedef struct {
+	char *name;
+	uint8_t data;
+	time_t timestamp;
+	uint8_t freshness;
+} Sensor_Data;
+
+
+typedef struct {
+	Sensor_Data pressure;
+	Sensor_Data temperature;	
+} Pod_Data_Handle;
 
 
 #endif
