e23ad769920ebe3f29ce0ec9e3f7739d8931ae7a
vkottler <vkottler@wisc.edu>
2018-07-08 02:50:43 -0500

    mostly formatting, some slight changes. Larger refactors to come

--- embedded/common/commands/can.c
+++ embedded/common/commands/can.c
@@ -2,51 +2,47 @@
 #include <string.h>
 #include <stdlib.h>
 #include <limits.h>
-
 #include "commands.h"
 #include "can.h"
 
-extern CAN_TxHeaderTypeDef TxHeader;
-extern CAN_RxHeaderTypeDef RxHeader;
-extern uint8_t RxData[8];
-extern uint8_t TxData[8];
-extern CAN_HandleTypeDef can_handle;
-
-command_status can_try_init(void){
-	return (can_init() == HAL_OK) ? CMD_SUCCESS: FAIL;
+command_status try_can_init(void) {
+	return (can_init() == HAL_OK) ? CMD_SUCCESS : FAIL;
 }
 
-command_status try_send(uint32_t can_id, size_t length, uint8_t *TxData){
+command_status try_send(uint32_t can_id, size_t length, uint8_t *TxData) {
 	if (can_send(can_id, length, TxData) != HAL_OK)
 		return FAIL;
-
 	return CMD_SUCCESS;	
 }
-command_status can_try_read(void){
+
+command_status can_try_read(void) {
+
+	uint8_t i;
 	HAL_StatusTypeDef retval = FAIL;
-	if (HAL_CAN_GetRxFifoFillLevel(&can_handle, CAN_RX_FIFO0)){
+
+	if (HAL_CAN_GetRxFifoFillLevel(&can_handle, CAN_RX_FIFO0)) {
 		retval = HAL_CAN_GetRxMessage(& can_handle, CAN_RX_FIFO0, &RxHeader, RxData);
 		           
 		/* Printing out received data */
 		printf("Received CAN ID: #%lx\r\n", RxHeader.StdId );
-		uint8_t i;
-		for(i = 0; i < 8; i++){
-			if( RxData[i] != 0){
+		for (i = 0; i < 8; i++) {
+			if (RxData[i] != 0) {
 				printf("CAN Message Data #%d: %x\r\n", i, RxData[i]);
 			}
 		}
 	}
+
 	return (retval == HAL_OK) ? CMD_SUCCESS : FAIL;	
 }
 
 command_status do_can(int argc, char *argv[]) {
-	printf("%s: TODO (%d args given)\r\n", argv[0], argc);
 	
 	if (argc == 1) return USAGE;
 
 	/* init (or re-init) the can subsystem */
 	if (!strcmp("init", argv[1]))
-		return can_try_init();
+		return try_can_init();
+
 	/* read a can message */
 	if (!strcmp("read", argv[1]))
 		return can_try_read();
@@ -56,17 +52,17 @@ command_status do_can(int argc, char *argv[]) {
 		//TEST MESSAGE SEND
 		uint32_t can_id = 0x123;
 		uint8_t length = 8;
-
 		uint64_t data = 0x5555555555555555;		
 		uint8_t TxData[8];
 		memset(TxData, data, 16);
 
+		// TODO, use 'data' as-is
 		return try_send(can_id, length, TxData);
 	}
 	
 	if (argc == 2 || argc == 3 || argc == 4) return USAGE;
 
-	if (!strcmp("send", argv[1])){
+	if (!strcmp("send", argv[1])) {
 		
 		char * str;
 		uint32_t can_id = strtol(argv[2], &str, 16);
@@ -76,13 +72,16 @@ command_status do_can(int argc, char *argv[]) {
 
 		uint8_t data[8];
 		long long converted = strtoll(argv[3], &str, 16);
-		if (converted == LONG_MAX || converted == LONG_MIN ){
+
+		if (converted == LONG_MAX || converted == LONG_MIN) {
 			printf("strtoll() error.\r\n" );
 		}
-		else if (str == argv[3] ){
+
+		else if (str == argv[3]) {
 			printf("No digits were found.\r\n");
 		}
-		else if (*str != '\0'){
+
+		else if (*str != '\0') {
 			printf("Extra string passed in.\r\n");
 		}
 		
@@ -103,7 +102,11 @@ command_status do_can(int argc, char *argv[]) {
 
 COMMAND_ENTRY(
 	"can",
-	"can send",
-	"can read",
+	"can <subcommand> [args...]\r\n\r\n"
+	"init\r\n"
+	"read\r\n"
+	"send_test\r\n"
+	"send <TODO>\r\n",
+	"Interact with the CAN subsystem.",
 	do_can
 )
