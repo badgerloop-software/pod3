046375a4b5d874411ded79e5c2f8210555023c89
vkottler <vkottler@wisc.edu>
2018-07-08 02:50:43 -0500

    mostly formatting, some slight changes. Larger refactors to come

--- embedded/drivers/can.c
+++ embedded/drivers/can.c
@@ -10,74 +10,81 @@ uint8_t TxData[8];
 uint8_t RxData[8];
 extern uint8_t board_num;
 
-uint32_t can_message_available( uint32_t RxFifo){
+uint32_t can_message_available(uint32_t RxFifo) {
 	return HAL_CAN_GetRxFifoFillLevel(&can_handle, RxFifo);
 }
 
-HAL_StatusTypeDef can_send(uint32_t can_id, size_t length, uint8_t *TxData){
+HAL_StatusTypeDef can_send(uint32_t can_id, size_t length, uint8_t *TxData) {
+
+	uint32_t TxMailbox = 0;
+
 	HAL_StatusTypeDef retval = HAL_ERROR;
 	TxHeader.StdId = can_id;
 	TxHeader.IDE = 0;
 	TxHeader.RTR = 0;
-	if (length % 2 == 1){
-		TxHeader.DLC = (length/2) + 1;
+
+	/* TODO: why are we doing this? */
+	if (length % 2 == 1) {
+		TxHeader.DLC = (length / 2) + 1;
 	} else {
-		TxHeader.DLC = length/2;
+		TxHeader.DLC = length / 2;
 	}
-	int val = HAL_CAN_GetTxMailboxesFreeLevel(&can_handle);
-	printf("CAN TxMailboxesfree level %d", val);
+
+	//printf("CAN TxMailboxesfree level %d", val);
 	
-	if (val){
+	if (HAL_CAN_GetTxMailboxesFreeLevel(&can_handle)) {
 		printf("SENDING MESSAGE\r\n");
-		uint32_t TxMailbox = 0;
 		retval = HAL_CAN_AddTxMessage(&can_handle, &TxHeader, TxData, &TxMailbox);
-		if (retval != HAL_OK) return retval;
-							
+		if (retval != HAL_OK)
+			return retval;
 	} else return HAL_ERROR;
+
 	return retval;
 }
 
-HAL_StatusTypeDef can_read(void){
-	HAL_StatusTypeDef retval = HAL_ERROR;
+HAL_StatusTypeDef can_read(void) {
+
 	int i;
-	if(can_message_available(CAN_RX_FIFO0)){
+	HAL_StatusTypeDef retval = HAL_ERROR;
+
+	if (can_message_available(CAN_RX_FIFO0)) {
 		printf("CAN Message Received.\r\n");
-		retval =HAL_CAN_GetRxMessage(&can_handle, CAN_RX_FIFO0, &RxHeader, RxData);
+		retval = HAL_CAN_GetRxMessage(&can_handle, CAN_RX_FIFO0, &RxHeader, RxData);
 		           
 		/* Printing out received data */
 		printf("Received CAN ID: #%lx\r\n", RxHeader.StdId );
-		for (i = 0; i < 8; i++){
-			if (RxData[i] != 0){
-				printf("CAN Message Data #%d: %x\r\n", i, RxData[i]);
+		for (i = 0; i < 8; i++) {
+			if (RxData[i] != 0) {
+				printf("CAN Message Data[%d]: %x\r\n", i, RxData[i]);
 			}
 		}
 	}
 	return retval;
 }
 
-HAL_StatusTypeDef can_init(void){
+HAL_StatusTypeDef can_init(void) {
+
 	HAL_StatusTypeDef retval;	
 	CAN_FilterTypeDef sFilterConfig0;
-	/* CAN */
-	HAL_Init();
+
 	/* Clock Enables */
 	__HAL_RCC_CAN1_CLK_ENABLE();
-	__HAL_RCC_GPIOA_CLK_ENABLE();
+
 	/* General CAN Init */
-	can_handle.Instance = CAN1;
-	can_handle.Init.TimeTriggeredMode = DISABLE;
-	can_handle.Init.AutoBusOff = DISABLE;
-	can_handle.Init.AutoWakeUp = ENABLE;
-	can_handle.Init.AutoRetransmission = ENABLE;
-	can_handle.Init.ReceiveFifoLocked = DISABLE;
-	can_handle.Init.TransmitFifoPriority = DISABLE;
-	can_handle.Init.Mode = CAN_MODE_NORMAL;
+	can_handle.Instance						= CAN1;
+	can_handle.Init.TimeTriggeredMode		= DISABLE;
+	can_handle.Init.AutoBusOff				= DISABLE;
+	can_handle.Init.AutoWakeUp				= ENABLE;
+	can_handle.Init.AutoRetransmission		= ENABLE;
+	can_handle.Init.ReceiveFifoLocked		= DISABLE;
+	can_handle.Init.TransmitFifoPriority	= DISABLE;
+	can_handle.Init.Mode					= CAN_MODE_NORMAL;
 
 	/* CAN Bit Timing Register Init */
-	can_handle.Init.SyncJumpWidth = CAN_SJW_1TQ;
-	can_handle.Init.TimeSeg1 = CAN_BS1_4TQ;
-	can_handle.Init.TimeSeg2 = CAN_BS2_3TQ;
-	can_handle.Init.Prescaler = 12;
+	can_handle.Init.SyncJumpWidth			= CAN_SJW_1TQ;
+	can_handle.Init.TimeSeg1				= CAN_BS1_4TQ;
+	can_handle.Init.TimeSeg2				= CAN_BS2_3TQ;
+
 	/* Prescaler Calc 
 	 * Figure 452 Page 1369 of the Reference manual
 	 *
@@ -88,17 +95,19 @@ HAL_StatusTypeDef can_init(void){
 	 * Tq = (Prescaler + 1) * Tpclk
 	 *
 	 */
+	can_handle.Init.Prescaler				= 12;
 
 	/* CAN Filter Config */
-	sFilterConfig0.FilterBank = 0;
-	sFilterConfig0.FilterMode = CAN_FILTERMODE_IDMASK;
-	sFilterConfig0.FilterScale = CAN_FILTERSCALE_32BIT;
-	sFilterConfig0.FilterFIFOAssignment = CAN_RX_FIFO0;
-	sFilterConfig0.SlaveStartFilterBank = 14;
+	sFilterConfig0.FilterBank				= 0;
+	sFilterConfig0.FilterMode				= CAN_FILTERMODE_IDMASK;
+	sFilterConfig0.FilterScale				= CAN_FILTERSCALE_32BIT;
+	sFilterConfig0.FilterFIFOAssignment		= CAN_RX_FIFO0;
+	sFilterConfig0.SlaveStartFilterBank		= 14;
 	
-	/* Board Specific (Filter) Initialization */
-	/* For CAN ID filtering, look at: https://community.st.com/thread/28968 */
-	/*
+	/* Board Specific (Filter) Initialization
+	 *
+	 * For CAN ID filtering, look at: https://community.st.com/thread/28968
+	 *
 	 * For an incomming CAN_ID:
 	 * Bits [11:8] determine the FROM node
 	 * Bits [7:4] determine the TO node(s)
@@ -111,32 +120,34 @@ HAL_StatusTypeDef can_init(void){
 	 * DEV:		ALL
 	 *
 	 */
-
 	
 	switch (board_num) {
 		case DASH:
-			sFilterConfig0.FilterIdHigh = 0x0000;
-			sFilterConfig0.FilterIdLow = 0x0000;
-			sFilterConfig0.FilterMaskIdHigh = 0x0000;
-			sFilterConfig0.FilterMaskIdLow = 0x0000;
+			sFilterConfig0.FilterIdHigh			= 0x0000;
+			sFilterConfig0.FilterIdLow			= 0x0000;
+			sFilterConfig0.FilterMaskIdHigh 	= 0x0000;
+			sFilterConfig0.FilterMaskIdLow		= 0x0000;
+			break;
 		case NAV:
-			sFilterConfig0.FilterIdHigh = 0x0000;
-			sFilterConfig0.FilterIdLow = 0x0000;
-			sFilterConfig0.FilterMaskIdHigh = 0x0000;
-			sFilterConfig0.FilterMaskIdLow = 0x0000;
+			sFilterConfig0.FilterIdHigh			= 0x0000;
+			sFilterConfig0.FilterIdLow			= 0x0000;
+			sFilterConfig0.FilterMaskIdHigh		= 0x0000;
+			sFilterConfig0.FilterMaskIdLow		= 0x0000;
+			break;
 		case PV:
-			sFilterConfig0.FilterIdHigh = 0x0000;
-			sFilterConfig0.FilterIdLow = 0x0000;
-			sFilterConfig0.FilterMaskIdHigh = 0x0000;
-			sFilterConfig0.FilterMaskIdLow = 0x0000;
+			sFilterConfig0.FilterIdHigh			= 0x0000;
+			sFilterConfig0.FilterIdLow			= 0x0000;
+			sFilterConfig0.FilterMaskIdHigh		= 0x0000;
+			sFilterConfig0.FilterMaskIdLow		= 0x0000;
+			break;
 		case DEV:
-			sFilterConfig0.FilterIdHigh = 0x7ff << 5;
-			sFilterConfig0.FilterIdLow = 0x0000;
-			sFilterConfig0.FilterMaskIdHigh = 0x7ff << 5;
-			sFilterConfig0.FilterMaskIdLow = 0x0000;
+			sFilterConfig0.FilterIdHigh			= 0x7ff << 5;
+			sFilterConfig0.FilterIdLow			= 0x0000;
+			sFilterConfig0.FilterMaskIdHigh		= 0x7ff << 5;
+			sFilterConfig0.FilterMaskIdLow		= 0x0000;
+			break;
 		default:
 			return HAL_ERROR;
-
 	}
 	
 	sFilterConfig0.FilterActivation = ENABLE;
@@ -146,7 +157,7 @@ HAL_StatusTypeDef can_init(void){
 	if (retval != HAL_OK) return retval;
 
 	retval = HAL_CAN_ConfigFilter(&can_handle, &sFilterConfig0);
-	if (retval != HAL_OK )return retval;
+	if (retval != HAL_OK) return retval;
 
 	retval = HAL_CAN_Start(&can_handle);
 	if (retval != HAL_OK) return retval; 	
--- embedded/include/can.h
+++ embedded/include/can.h
@@ -5,61 +5,61 @@
 #include <stdbool.h>
 #include "stm32l4xx_hal_conf.h"
 
-extern CAN_HandleTypeDef     hcan;
-extern CAN_TxHeaderTypeDef   TxHeader;
-extern CAN_RxHeaderTypeDef   RxHeader;
-extern uint8_t               TxData[8];
-extern uint8_t               RxData[8];
-extern uint32_t TxMailbox;
+extern CAN_HandleTypeDef   hcan;
+extern CAN_TxHeaderTypeDef TxHeader;
+extern CAN_RxHeaderTypeDef RxHeader;
+extern uint8_t             TxData[8];
+extern uint8_t             RxData[8];
+extern uint32_t            TxMailbox;
 
 HAL_StatusTypeDef can_read(void);
 HAL_StatusTypeDef can_send(uint32_t can_id, size_t length, uint8_t *TxData);
 HAL_StatusTypeDef can_init(void);
 
-typedef enum sending_module_val{
-	RMS_SEND = 0,
- 	CCP_SEND = 1,
-	NAV_SEND = 2,
-	PV_SEND = 4,
-	BMS_SEND = 7
+typedef enum sending_module_val {
+	RMS_SEND	= 0x00,
+ 	CCP_SEND	= 0x01,
+	NAV_SEND	= 0x02,
+	PV_SEND		= 0x04,
+	BMS_SEND	= 0x07
 } SENDING_MODULE;
 
-typedef enum receiving_module_val{
-	RMS_REC_1 = 0xC,
-	RMS_REC_2 = 0xA,
-	CCP_REC = 1,
-	NAV_REC = 2,
-	PV_REC = 3,
-	BMS_REC = 0xe,
-	ALL_REC = 4,
-	CCP_NAV_REC = 5,
-	CCP_PV_REC = 6,
-	NAV_PV_REC = 7,
-	CCP_NAV_PV_REC =8
+typedef enum receiving_module_val {
+	RMS_REC_1		= 0x0C,
+	RMS_REC_2		= 0x0A,
+	CCP_REC			= 0x01,
+	NAV_REC			= 0x02,
+	PV_REC			= 0x03,
+	BMS_REC			= 0x0E,
+	ALL_REC			= 0x04,
+	CCP_NAV_REC		= 0x05,
+	CCP_PV_REC		= 0x06,
+	NAV_PV_REC		= 0x07,
+	CCP_NAV_PV_REC	= 0x08
 } RECEIVING_MODULE;
 
 typedef enum message_type {
-	CCP_FAULT = 0,
-	CCP_WARNING = 1,
-	LV_HEARTBEAT = 2,
-	NAV_SOLENOID_COMMAND = 3,
-	PV_MCU_ENABLE_COMMAND = 4,
-	PV_FAULT =5,
-	PV_WARNING =6,
-	SHUTDOWN_CIRCUIT_STATUS=7,
-	PV_PRESSURE=8,
+	CCP_FAULT				= 0,
+	CCP_WARNING				= 1,
+	LV_HEARTBEAT 			= 2,
+	NAV_SOLENOID_COMMAND 	= 3,
+	PV_MCU_ENABLE_COMMAND 	= 4,
+	PV_FAULT 				= 5,
+	PV_WARNING 				= 6,
+	SHUTDOWN_CIRCUIT_STATUS	= 7,
+	PV_PRESSURE				= 8,
 	// RMS_ENABLE_HEARTBEAT
 	// RMS_CLEAR_FAULTS
 	// RMS_TORQUE
 	// RMS_DISABLE_INVERTER
 	// RMS_CAP_DISCHARGE
-	NAV_FAULT=9,
-	NAV_WARNING=10,
-	TAPE_SHOULD_STOP=11,
-	NAV_PRES_1=12,
-	NAV_PRES_2=13,
-	NAV_PRES_3=14,
-	NAV_SOLENOID_1=15
+	NAV_FAULT				= 9,
+	NAV_WARNING				= 10,
+	TAPE_SHOULD_STOP		= 11,
+	NAV_PRES_1				= 12,
+	NAV_PRES_2				= 13,
+	NAV_PRES_3				= 14,
+	NAV_SOLENOID_1			= 15
 	//OBDII_RETURN
 } MESSAGE_TYPE;
 
