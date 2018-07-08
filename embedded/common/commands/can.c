56ba6051a355b93cc4dc9fee51facfa1a966977a
Ryan Castle <rcastle@wisc.edu>
2018-07-08 00:03:47 +0000

    serial commands and crappy can send function call

--- embedded/common/commands/can.c
+++ embedded/common/commands/can.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
+#include <limits.h>
 
 #include "commands.h"
 #include "can.h"
@@ -14,7 +16,6 @@ command_status can_try_init(void){
 	return(can_init() == HAL_OK) ? CMD_SUCCESS: FAIL;
 }
 
-//TODO
 command_status try_send(uint32_t can_id, size_t length, uint8_t *TxData){
 	if(can_send(can_id, length, TxData) != HAL_OK)
 		return FAIL;
@@ -50,11 +51,8 @@ command_status do_can(int argc, char *argv[]) {
 	if(!strcmp("read", argv[1]))
 		return can_try_read();
 	
-	//if(argc == 2 || argc == 3 || argc == 4) return USAGE;
-
-	/* send a can message */
-	
-	if(!strcmp("send", argv[1])){
+	/* send a can test message */
+	if(!strcmp("send_test", argv[1])){
 		//TEST MESSAGE SEND
 		uint32_t can_id = 0x123;
 		uint8_t length = 8;
@@ -70,6 +68,42 @@ command_status do_can(int argc, char *argv[]) {
 		return try_send(can_id, length, TxData);
 	}
 	
+	if(argc == 2 || argc == 3 || argc == 4) return USAGE;
+
+	if(!strcmp("send", argv[1])){
+		
+		char * str;
+		uint32_t can_id = strtol(argv[2], &str, 16);
+		if(can_id > 0x7ff) return ERROR;
+		
+		size_t length = strlen(argv[3]);
+
+		uint8_t data[8];
+		long long converted = strtoll(argv[3], &str, 16);
+		if(converted == LONG_MAX || converted == LONG_MIN ){
+			printf("strtoll() error.\r\n" );
+		}
+		else if( str == argv[3] ){
+			printf("No digits were found.\r\n");
+		}
+		else if( *str != '\0'){
+			printf("Extra string passed in.\r\n");
+		}
+		
+		data[7] = (converted & 0xff00000000000000) >> 56;
+		data[6] = (converted & 0x00ff000000000000) >> 48;
+		data[5] = (converted & 0x0000ff0000000000) >> 40;
+		data[4] = (converted & 0x000000ff00000000) >> 32;
+		data[3] = (converted & 0x00000000ff000000) >> 24;
+		data[2] = (converted & 0x0000000000ff0000) >> 16;
+		data[1] = (converted & 0x000000000000ff00) >> 8;
+		data[0] = (converted & 0x00000000000000ff);
+
+
+
+		can_send(can_id, length, data);
+
+	}
 
 	return USAGE;
 }
