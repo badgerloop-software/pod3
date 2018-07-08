bc5a50aa7cf7e8aa7424c4f45553f97d07ab2838
Ryan Castle <rcastle@wisc.edu>
2018-07-07 22:01:58 -0500

    initial shutdown circuit telemetry reading

---
+++ embedded/drivers/shutdowncircuit.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+
+#include "gpio.h"
+#include "shutdowncircuit.h"
+
+void mcu_shutdown_circuit(int state){
+	gpio_writePin(GPIOA, 6, state);
+}
+
+void rms_power(int state){
+	gpio_writePin(GPIOA, 5, state);
+}
+
+void read_shutdown_circuit_status(void){
+	uint8_t temp = -1;
+	temp = gpio_readPin(GPIOB, 0);
+	printf("TEMP 1 %d", temp);
+	if(temp) SET_MSTR_SW_FDBK;
+	else CLR_MSTR_SW_FDBK;
+
+	temp = gpio_readPin(GPIOB, 7);
+	printf("TEMP 2 %d", temp);
+	if(temp) SET_E_STOP_FDBK;
+	else CLR_E_STOP_FDBK;
+
+	temp = gpio_readPin(GPIOB, 6);
+	printf("TEMP 3 %d", temp);
+	if(temp) SET_PV_LIM_FDBK;
+	else CLR_PV_LIM_FDBK;
+
+	temp = gpio_readPin(GPIOB, 1);
+	printf("TEMP 4 %d", temp);
+	if(temp) SET_HVD_FDBK;
+	else CLR_HVD_FDBK;
+
+	temp = gpio_readPin(GPIOC, 14);
+	printf("TEMP 5 %d", temp);
+	if(temp) SET_BMS_STAT_FDBK;
+	else CLR_BMS_STAT_FDBK;
+
+	temp = gpio_readPin(GPIOC, 8);
+	printf("TEMP 6 %d", temp);
+	if(temp) SET_IMD_STAT_FDBK;
+	else CLR_IMD_STAT_FDBK;
+	
+	temp = gpio_readPin(GPIOA, 8);
+	printf("TEMP 7 %d", temp);
+	if(temp) SET_HV_EN_FDBK;
+	else CLR_HV_EN_FDBK;
+}
---
+++ embedded/include/shutdowncircuit.h
@@ -0,0 +1,38 @@
+#ifndef _SHUTDOWNCIRCUIT__H__
+#define _SHUTDOWNCIRCUIT__H__
+
+void muc_shutdown_circuit(int state);
+void rms_power(int state);
+void read_shutdown_circuit_status(void);
+
+extern uint8_t *shutdown_circuit_status;
+#define MSTR_SW_FDBK_TEL 0x1
+#define SET_MSTR_SW_FDBK	*shutdown_circuit_status |= MSTR_SW_FDBK_TEL
+#define CLR_MSTR_SW_FDBK	*shutdown_circuit_status &= ~MSTR_SW_FDBK_TEL
+#define GET_MSTR_SW_FDBK	(*shutdown_circuit_status & MSTR_SW_FDBK_TEL)
+#define E_STOP_FDBK_TEL 0x2
+#define SET_E_STOP_FDBK		*shutdown_circuit_status |= E_STOP_FDBK_TEL
+#define CLR_E_STOP_FDBK		*shutdown_circuit_status &= ~E_STOP_FDBK_TEL
+#define GET_E_STOP_FDBK		(*shutdown_circuit_status & E_STOP_FDBK_TEL)
+#define PV_LIM_FDBK_TEL 0x4
+#define SET_PV_LIM_FDBK		*shutdown_circuit_status |=PV_LIM_FDBK_TEL
+#define CLR_PV_LIM_FDBK		*shutdown_circuit_status &= ~PV_LIM_FDBK_TEL
+#define GET_PV_LIM_FDBK		(*shutdown_circuit_status & PV_LIM_FDBK_TEL)
+#define HVD_FDBK_TEL 0x8
+#define SET_HVD_FDBK		*shutdown_circuit_status |= HVD_FDBK_TEL
+#define CLR_HVD_FDBK		*shutdown_circuit_status &= ~HVD_FDBK_TEL
+#define GET_HVD_FDBK		(*shutdown_circuit_status & HVD_FDBK_TEL)
+#define BMS_STAT_FDBK_TEL 0x10
+#define SET_BMS_STAT_FDBK	*shutdown_circuit_status |= BMS_STAT_FDBK_TEL
+#define CLR_BMS_STAT_FDBK	*shutdown_circuit_status &= ~BMS_STAT_FDBK_TEL
+#define GET_BMS_STAT_FDK	(*shutdown_circuit_statuss & BMS_STAT_FDBK_TEL)
+#define IMD_STAT_FDBK_TEL 0x20
+#define SET_IMD_STAT_FDBK	*shutdown_circuit_status |= IMD_STAT_FDBK_TEL
+#define CLR_IMD_STAT_FDBK	*shutdown_circuit_status &= ~IMD_STAT_FDBK_TEL
+#define GET_IMD_STAT_FDBK	(*shutdown_circuit_status & IMD_STAT_FDBK_TEL)
+#define HV_EN_FDBK_TEL 0x40
+#define SET_HV_EN_FDBK		*shutdown_circuit_status |= HV_EN_FDBK_TEL
+#define CLR_HV_EN_FDBK		*shutdown_circuit_status &= ~HV_EN_FDBK_TEL
+#define GET_HV_EN_FDBK		(*shutdown_circuit_status & HV_EN_FDBK_TEL)
+
+#endif
