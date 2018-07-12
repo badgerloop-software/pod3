b24c100eff3ee3d577e4f5e92c29ae1f3dacf622
Ethan Link <ethanlink17@live.com>
2018-07-11 22:50:54 -0500

    Testing for i2c adcs

--- embedded/devices/pcf8591.c
+++ embedded/devices/pcf8591.c
@@ -13,14 +13,17 @@ bool adcx_start_read(uint8_t addr){
 bool adcx_read(uint8_t addr, uint8_t * val) {
 		
 	_i2cadc_read_pos = *val;
-	
+	_i2cadc_read_val = 0;
+
 	printf("in function\r\n");
 
 	if (i2c_start_write(addr, 1, &_i2cadc_read_pos) == HAL_OK){
 		if (!i2c_read_ready()) return false;
-		_i2cadc_read_val = i2c_rx[0];
+        if( i2c_start_read(addr, 2) != HAL_OK ) return false; 
+        _i2cadc_read_val = i2c_rx[0];
 		printf("i2c read value %x\r\n", _i2cadc_read_val);
-		*val = i2c_rx[0];
+		*val = i2c_rx[1];
 	}
 	return true;
 }
+
