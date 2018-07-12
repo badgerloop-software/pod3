4dea7a5504e78dc3cc486184ead75362f717b82b
Ezra Boley <eboley@wisc.edu>
2018-07-12 05:03:06 +0000

    Committing what I have for the night. Needs to be tested

--- embedded/devices/pcf8591.c
+++ embedded/devices/pcf8591.c
@@ -14,13 +14,14 @@ bool adcx_read(uint8_t addr, uint8_t * val) {
 		
 	_i2cadc_read_pos = *val;
 	_i2cadc_read_val = 0;
+	uint32_t ticks_start = ticks;
 
 	printf("in function\r\n");
 
 	if (i2c_start_write(addr, 1, &_i2cadc_read_pos) == HAL_OK){
 		if (!i2c_read_ready()) return false;
-        if( i2c_try_read(addr, 2) != HAL_OK ) return false; 
-		if (i2c_block(I2C_WAITING_TX, ticks_start) || i2c_errors_present()) return FAIL;
+        if( i2c_start_read(addr, 2) != HAL_OK ) return false; 
+		if (i2c_block(I2C_WAITING_RX, ticks_start) || i2c_errors_present()) return false;
 		_i2cadc_read_val = i2c_rx[0];
 		printf("i2c read value %x\r\n", _i2cadc_read_val);
 		*val = i2c_rx[1];
