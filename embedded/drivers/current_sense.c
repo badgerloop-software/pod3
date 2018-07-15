ef26a88753e1cec5ccc9bc3d796c0302e22bcc77
vkottler <vkottler@wisc.edu>
2018-07-15 17:03:43 -0500

    flushed out soc calcs

--- embedded/drivers/current_sense.c
+++ embedded/drivers/current_sense.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-#include "adc.h"
+#include <adc.h>
 #include <stdlib.h>
-#include "current_sense.h"
+#include <current_sense.h>
 
 extern ADC_HandleTypeDef adc_handle;
+extern uint8_t ticks;
+
+const uint32_t SOC_INITIAL = 1013760;
+
+uint32_t last_batt_timestamp;
+uint8_t soc; 
+uint8_t curr_draw, charge_perc, time_remaining;
 
 int current_sense_init(void){
     
@@ -36,3 +43,24 @@ double current_sense_read(void){
 	//return (double)(adc_read( CURRENT_SENSE_PIN ) * (double)(24.414) / 1000);
 }
 
+void state_of_charge_init(void){
+	soc = SOC_INITIAL;
+}
+
+void state_of_charge(void){
+        if ((!ticks % 1000) && last_batt_timestamp != ticks){
+                last_batt_timestamp = ticks;
+                curr_draw = (current_sense_read() * 14.1) / 1000000;
+
+                soc -= (curr_draw);
+                set_charge_percentage(((soc * 100) / SOC_INITIAL));
+        }
+}
+
+void set_charge_percentage(uint16_t state_of_charge){
+	charge_perc = ((state_of_charge * 100) / SOC_INITIAL);
+}
+
+uint16_t  get_time_remaining(void){
+	return ((curr_draw == 0) ? 1012651 : soc / curr_draw);
+}	             
--- embedded/include/current_sense.h
+++ embedded/include/current_sense.h
@@ -7,4 +7,6 @@
 
 int current_sense_init(void);
 double current_sense_read(void);
-
+void state_of_charge(void);
+void set_charge_percentage(uint16_t soc);
+uint16_t get_time_remaining(void);
