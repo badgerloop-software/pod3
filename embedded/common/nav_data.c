78420d83779dd725cd1c547723216d23740d43ca
Ezra Boley <eboley@wisc.edu>
2018-07-15 04:35:28 +0000

    solenoids can all be read and are stored in a struct

--- embedded/common/nav_data.c
+++ embedded/common/nav_data.c
@@ -1,8 +1,18 @@
 #include "nav_data.h"
 #include "solenoid.h"
-
+#include <stdio.h>
 int nav_DAQ(Nav_Data *navData) {
+	int temp = navData->solenoids.solenoid_1.state;
+	int temp2 = navData->solenoids.solenoid_2.state;
+	int temp3 = navData->solenoids.solenoid_4.state;
+	int temp4 = navData->solenoids.solenoid_6.state;
+	int temp5 = navData->solenoids.solenoid_7.state;
+
 	harvest_solenoids(&(navData->solenoids));	
-	
+	if (navData->solenoids.solenoid_1.state != temp) printf("CHANGE1\r\n");
+	if (navData->solenoids.solenoid_2.state != temp2) printf("CHANGE2\r\n"); 
+	if (navData->solenoids.solenoid_4.state != temp3) printf("CHANGE4\r\n");
+	if (navData->solenoids.solenoid_6.state != temp4) printf("CHANGE6\r\n");
+	if (navData->solenoids.solenoid_7.state != temp5) printf("CHANGE7\r\n");   	
 	return 0; // on Success
 }
--- embedded/common/solenoid.c
+++ embedded/common/solenoid.c
@@ -3,20 +3,21 @@
 #include "board.h"
 #include "uart.h"
 #include "usart.h"
-
+#include <stdio.h>
+ 
 #define START_READ_INTERVAL 1000
 // format --> 8 bit integer and MSB is Solenoid 7 to 0
 void harvest_solenoids(Solenoid_States *sol_states) {
-	iox_start_read();
+
 	uint8_t stateList;
-	if (iox_read(&stateList)) {
+	
+    if (iox_read(&stateList)) {
 		sol_states->solenoid_7.state = (stateList & 0x40) >> 6;
-		sol_states->solenoid_6.state = (stateList & 0x20) >> 5;
-		/*sol_states->solenoid_5.state = (stateList & 0x10) >> 4; NOT WIRED*/
+    	sol_states->solenoid_6.state = (stateList & 0x20) >> 5;
+	 	/*sol_states->solenoid_5.state = (stateList & 0x10) >> 4; NOT WIRED*/
 		sol_states->solenoid_4.state = (stateList & 0x08) >> 3;
 		/*sol_states->solenoid_3.state = (stateList & 0x04) >> 2; NOT WIRED*/
 		sol_states->solenoid_2.state = (stateList & 0x02) >> 1;
 		sol_states->solenoid_1.state = (stateList & 0x01);
-	};
-	printf("SOL_7 = %u SOL_6 = %u SOL_4 = %u SOL_2 = %u SOL_1 = %u \r\n", sol_states->solenoid_7.state, sol_states->solenoid_6.state, sol_states->solenoid_4.state, sol_states->solenoid_2.state,sol_states->solenoid_1.state);
+	}
 }
