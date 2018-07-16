b8bd8459643f09542f2d9446554e7ee0b612ce5e
Ethan Link <ethanlink17@live.com>
2018-07-16 02:00:26 -0500

    Initial work for adding Limit switch data sending

--- embedded/common/nav_data.c
+++ embedded/common/nav_data.c
@@ -1,12 +1,9 @@
 #include "nav_data.h"
 #include "solenoid.h"
 #include <stdio.h>
-<<<<<<< 10f4d5e586b98bec434f847c7c2c008e53d0103a
 #include "retro.h"
-=======
 #include "gpio.h"
 
->>>>>>> Initial work for adding Limit switch data sending
 Nav_Data navData = {
      .solenoids = {
          .solenoid_1 = {PRIM_BRAKING_1, NOT_ACTUATED},
@@ -30,6 +27,10 @@ int nav_DAQ(Nav_Data *navDataPtr) {
 	int badRetroGuy;
 	navDataPtr->retros.retroAgreement = getStripCount(&badRetroGuy);
 	printf("Strip Count: %u\r\n", navDataPtr->retros.retroAgreement);
+
+    navDataPtr->limits.limitValue1 = gpio_readPin( GPIOA, 3);
+    navDataPtr->limits.limitValue2 = gpio_readPin( GPIOA, 6);
+    navDataPtr->limits.limitValue3 = gpio_readPin( GPIOA, 7);
 
     navDataPtr->limits.limitValue1 = gpio_readPin( GPIOA, 3);
     navDataPtr->limits.limitValue2 = gpio_readPin( GPIOA, 6);
