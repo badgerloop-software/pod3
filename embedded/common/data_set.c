65ddabab602f395ff62b3d971fa50fb6ff42a8f5
Ethan Link <ethanlink17@live.com>
2018-07-16 02:04:57 -0500

    More limit switch stuff

--- embedded/common/data_set.c
+++ embedded/common/data_set.c
@@ -13,16 +13,25 @@ extern Nav_Data navData;
  * FROM: NAV
  *
  * Data Byte 2: Voted Retro Count
+<<<<<<< 2ffc22610a003595eb429f2c327c9ccb0f9774ed
  * Data Byte 3: Lim 1
  * Data Byte 4: Lim 2
  * Data Byte 5: Lim 3
+=======
+ * Data Byte 3: Limit Switch 1 GPIO Value
+ * Data Byte 4: Limit Switch 2 GPIO Value
+ * Data Byte 5: Limit Switch 3 GPIO Value
+>>>>>>> More limit switch stuff
  * Data Byte 6: 0
  * Data Byte 7: 0 */
 void nav_tape_set(uint8_t data[]){
     int i;
     //TODO: Do error checking with badRetro
     data[2] = navData.retros.retroAgreement;
-    //data[3] = interLine[3].count;
+    data[3] = navData.limits.limitValue1;
+    data[4] = navData.limits.limitValue2;
+    data[5] = navData.limits.limitValue3;
+
     for ( i = 3; i < 8; i++ ){
         data[i] = 0;
     }
