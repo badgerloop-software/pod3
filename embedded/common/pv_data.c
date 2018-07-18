fbcadf1f2f147b6491b33e0591fe53ce60eae3e9
Ezra Boley <eboley@wisc.edu>
2018-07-17 22:33:32 -0700

    working on honeywell

--- embedded/common/pv_data.c
+++ embedded/common/pv_data.c
@@ -6,15 +6,9 @@
 	////pv_pressure_reader(pvData);
 //	return 0;
 //}
-/*
-int MCU_reader(PV_Data *pvData) {
-	//Read MCU info and store
-	return 0;
+
+
+int pv_harvest_honeywell(PV_Data *pvData) {
+	int pres, temp;
+
 }
-*/
-/*
-int pv_pressure_reader(PV_Data *pvData) {
-	//Read ADCs and then collect the data into the struct
-	return 0;
-}
-*/
