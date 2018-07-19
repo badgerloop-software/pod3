1fe26d13f72c4d6adab8ebebda126d077a19567c
Ethan Link <ethanlink17@live.com>
2018-07-19 09:18:10 -0500

    ADCx initial work

--- embedded/include/adcx.h
+++ embedded/include/adcx.h
@@ -7,8 +7,8 @@
 #define PCF8591_1_ADDR 0x48
 #define PCF8591_2_ADDR 0x49
 
-bool adcx_start_read(uint8_t addr);
-bool adcx_read(uint8_t addr, uint8_t * val);
+bool adcx_start_read(uint8_t addr, uint8_t channel );
+bool adcx_read(uint16_t * val);
 
 
 #endif
