0996ea3f90769723ae81238e0b815ba6bbba7a55
Ezra P Boley <eboley@wisc.edu>
2018-07-19 14:46:08 +0000

    Debugging

--- embedded/include/adcx.h
+++ embedded/include/adcx.h
@@ -7,7 +7,7 @@
 #define PCF8591_1_ADDR 0x48
 #define PCF8591_2_ADDR 0x49
 
-bool adcx_start_read(uint8_t addr, uint8_t channel );
+bool adcx_start_read(uint8_t addr, uint8_t* channel );
 bool adcx_read(uint16_t * val);
 
 
